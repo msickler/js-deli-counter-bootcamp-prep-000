3cec998861b9af617029d7b667918b4a9e61a56e
Mariana Sickler <marianaleal_93@hotmail.com>
2017-09-05 17:42:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var katzDeli = [];
 
-function takeANumber(katzDeli, name) {
-  katzDeli.push(name);
+function takeANumber(katzDeliLine, name) {
+  katzDeliLine.push(name);
   return (`Welcome, ${name}. You are number ${katzDeliLine.length} in line.`);
 }
 
